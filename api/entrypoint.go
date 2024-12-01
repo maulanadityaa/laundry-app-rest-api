477b9813fda207d42cb89b8b453d32b634899d25
maulanadityaa <maulanadityaaa@gmail.com>
2024-12-01 12:28:55 +0700

    [UPDATE] debug environment vercel

--- api/entrypoint.go
+++ api/entrypoint.go
@@ -1,21 +1,18 @@
 package api
 
 import (
-	"log"
 	"net/http"
 
 	"maulanadityaa/laundry-app-rest-api/app"
-
-	"github.com/joho/godotenv"
 )
 
 var router = app.InitApp()
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
 
 	router.ServeHTTP(w, r)
 }
