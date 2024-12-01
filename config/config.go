7949e475bb3c807ebbe40e9b5ef8361c5af73c25
maulanadityaa <maulanadityaaa@gmail.com>
2024-12-01 12:04:58 +0700

    [UPDATE] debug environment vercel

--- config/config.go
+++ config/config.go
@@ -10,7 +10,7 @@ import (
 func LoadConfig() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file", err.Error())
+		log.Fatal(err.Error())
 	}
 
 	// Access environment variables
