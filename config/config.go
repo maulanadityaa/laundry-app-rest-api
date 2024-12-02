ff35d5f2b544df298496128a190257d3fcc10bd8
maulanadityaa <maulanadityaaa@gmail.com>
2024-12-02 11:50:10 +0700

    [UPDATE] change db config for vercel

--- config/config.go
+++ config/config.go
@@ -3,15 +3,13 @@ package config
 import (
 	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func LoadConfig() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
 
 	// Access environment variables
 	dbHost := os.Getenv("DB_HOST")
